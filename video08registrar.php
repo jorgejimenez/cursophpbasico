1ebe388e197b09fd55e89da4515df0d8aac09153
Jorge Luis <jorge.luis.jimenezm@gmail.com>
2013-10-30 14:31:37 -0430

    Create video08registrar.php

---
+++ video08registrar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8" />
+    <title>XadrijoLab 08</title>
+</head>
+
+<body>
+
+<div>
+
+	<form action="procesar.php" method="POST">
+
+		<p>Usuario: <input type="text" name="usuario" size="20"></p>
+		<p>Contraseña: <input type="password" name="password" size="20"></p>
+		<p>Confirmar Contraseña: <input type="password" name="confirm" size="20"></p>
+
+		<p><input type="checkbox" name="terminos" value="si">Acepta los tèrminos</p>
+		<p><input type="submit" name="submit" value="Registrar"></p>
+
+	</form>
+
+</div>
+
+
+
+</body>
+</html>
