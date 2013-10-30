4c4862626e5de04142dcb89d88d7b142875b2326
Jorge Luis <jorge.luis.jimenezm@gmail.com>
2013-10-30 14:32:37 -0430

    Create video09registro.php

---
+++ video09registro.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>XadrijoLab 09</title>
+</head>
+<body>
+
+	<form action="procesar.php" method="POST">
+		<label for="nombre">Nombre</label>
+		<input type="text" name="nombre" size="20"><br>
+		<label for= "edad">Edad</label>	
+		<input type="text" name= "edad" size="10"><br>
+
+		<input type="submit" value="Enviar">
+	</form>
+
+</body>
+</html>
