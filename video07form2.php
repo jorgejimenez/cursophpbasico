7645864031bc8e323e5d6918c8d67b5bd6e753ba
Jorge Luis <jorge.luis.jimenezm@gmail.com>
2013-10-30 14:27:35 -0430

    Create video07form2.php

---
+++ video07form2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8" />
+    <title>XadrijoLab 07</title>
+</head>
+
+<body>
+
+<div>
+
+<p>Click para recibir saludo.</p>
+
+<ul>
+
+</ul>
+	<li><a href="saludo.php?name=Jorge&lastname=Jimenez">Jorge</a></li>
+	<li><a href="saludo.php?name=Miguel&lastname=Vera">Miguel</a></li>
+	<li><a href="saludo.php?name=Jose&lastname=Rondon">Jose</a></li>
+</div>
+
+</body>
+</html>
