bdc14c193c0f5e3c4358ba63653c6084b787299b
Jorge Luis <jorge.luis.jimenezm@gmail.com>
2013-10-30 14:30:02 -0430

    Create video07saludo.php

---
+++ video07saludo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8" />
+    <title>XadrijoLab 07</title>
+</head>
+
+<body>
+
+<div>
+
+<?php # VideoTutorial 07
+
+$name = $_GET['name'];
+$lastname = $_GET['lastname'];
+
+echo "Hola <b>$name</b> $lastname";
+
+?>
+</div>
+
+</body>
+</html>
